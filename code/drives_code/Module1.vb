b4013ace8e38670bd4e81712f12b77a226141864
Milkey Mouse <sillystixco@hotmail.com>
2014-07-27 13:27:19 -0700

    Added code for drive control
    I also added a little stub of a tutorial for the drives.

---
+++ code/drives_code/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+
+Module Module1
+
+    <DllImport("winmm.dll")> _
+    Public Function mciSendString(lpstrCommand As String, lpstrReturnString As StringBuilder, uReturnLength As UInt32, hwndCallback As IntPtr) As UInt32
+    End Function
+
+    Public rt As StringBuilder
+
+    Sub Main()
+        mciSendString("open L: type cdaudio alias driveL", rt, 127, 0)
+        mciSendString("set driveL door closed", rt, 127, 0)
+        mciSendString("close driveL", rt, 127, 0)
+    End Sub
+
+End Module
