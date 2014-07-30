00576bd66a586518dc2f8f59cc2beb3c8886b2c7
Milkey Mouse <sillystixco@hotmail.com>
2014-07-30 11:22:03 -0700

    Added Bing Rewards scraper code
    Also added two pictures for the next tutorial

---
+++ code/rewards_code/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(400, 640)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("http://login.live.com/logout.srf", System.UriKind.Absolute)
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 640)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Watson Bing Extension"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ code/rewards_code/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        Try
+            If Me.WebBrowser1.Url.ToString().Contains("http://login.live.com/login.srf") Then
+                Me.WebBrowser1.Document.GetElementById("i0116").SetAttribute("value", "username")
+                Me.WebBrowser1.Document.GetElementById("i0118").SetAttribute("value", "password")
+                Me.WebBrowser1.Document.GetElementById("idChkBx_PWD_KMSI0Pwd").InvokeMember("click")
+                Me.WebBrowser1.Document.GetElementById("idSIButton9").InvokeMember("click")
+            End If
+            If Me.WebBrowser1.Url.ToString().Contains("http://www.bing.com/rewards/dashboard") Then
+                Me.Timer1.Start()
+            End If
+            If Me.WebBrowser1.Url.ToString().Contains("https://account.live.com/summarypage.aspx") Then
+                Me.WebBrowser1.Url = New Uri("http://www.bing.com/rewards/dashboard")
+            End If
+        Catch ex As Exception
+            If Not ex.Message = "Object reference not set to an instance of an object." Then Me.Close()
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            Dim attribute As String = Me.WebBrowser1.Document.GetElementById("id_rc").GetAttribute("innerText")
+            If File.Exists("bingPoints.txt") Then
+                File.Delete("bingPoints.txt")
+            End If
+            File.WriteAllText("bingPoints.txt", attribute)
+            Me.Close()
+        Catch
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub WebBrowser1_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles WebBrowser1.Navigating
+            If e.Url.ToString().Contains("msn.com") Then
+                Me.WebBrowser1.Url = New Uri("http://login.live.com/login.srf")
+            End If
+    End Sub
+End Class
