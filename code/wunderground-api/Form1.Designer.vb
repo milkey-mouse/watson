ee278828f28026a41c615399d501b8da3e365b83
Milkey Mouse <sillystixco@hotmail.com>
2014-08-04 18:39:57 -0700

    Aded Weather Underground signup tool
    With the code for it, of course.

---
+++ code/wunderground-api/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.WebControl1 = New Awesomium.Windows.Forms.WebControl(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer5 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer6 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer7 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ProgLabel = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer8 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "First Name"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(75, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Last Name"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(75, 38)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"United States", "Afghanistan", "Albania", "Alberta", "Algeria", "Angola", "Anguilla", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Beijing", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bolivia", "Bosnia", "Botswana", "Bouvet Island", "Brazil", "British Columbia", "British Indian Ocean", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burma/Myanmar", "Burundi", "Cambodia", "Cameroon", "Canada", "Canary Islands", "Canton Island", "Cape Verde", "Capital Territory", "Caroline Islands", "Cayman Island", "Central African Republic", "Chad", "Cheng-Du", "Chile", "China", "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Yemen", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "Gabon", "Gambia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guang-Zhou", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Han-Kou", "Hawaii", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Lan-Zhou", "Lao Peoples Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Luxembourg", "Macao", "Macedonia", "Madagascar", "Madeira Islands", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Manitoba", "Mariana Islands", "Marshall Islands", "Martinique", "Maryland", "Mauritania", "Mauritius", "Mexico", "Mongolia", "Montana", "Morocco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "New Brunswick", "New Caledonia", "New South Wales", "New Zealand", "Newfoundland", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Pacific Islands", "Norway", "Nova Scotia", "Oman", "Pakistan", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Prince Edward Island", "Puerto Rico", "Qatar", "Republic of Moldova", "Reunion Island", "Romania", "Russia", "Rwanda", "Saudi Arabia", "Senegal", "Seychelles", "Shang-Hai", "Shen-Yang", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "Southern Line Islands", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Tasmania", "Thailand", "Togo", "Tokelau Island", "Tonga", "Trinidad And Tobago", "Tunisia", "Turkey", "Turks Islands", "Tuvalu", "United States", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Urum-Qui", "Uzbekistan", "Vanuata", "Venezuela", "Victoria", "Viet Nam", "Virgin Islands", "Virginia", "Wake Island", "Wallis And Futuna Island", "Western Sahara", "Western Samoa", "Yemen", "Yugoslavia", "Yukon Territory", "Zambia", "Zimbabwe"})
+        Me.ComboBox1.Location = New System.Drawing.Point(75, 64)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Country"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 91)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(166, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'WebControl1
+        '
+        Me.WebControl1.Location = New System.Drawing.Point(0, 0)
+        Me.WebControl1.Size = New System.Drawing.Size(0, 0)
+        Me.WebControl1.Source = New System.Uri("about:blank", System.UriKind.Absolute)
+        Me.WebControl1.TabIndex = 9
+        Me.WebControl1.ViewType = Awesomium.Core.WebViewType.Offscreen
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2500
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 2500
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 2500
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 2500
+        '
+        'Timer5
+        '
+        Me.Timer5.Interval = 2500
+        '
+        'Timer6
+        '
+        Me.Timer6.Interval = 2500
+        '
+        'Timer7
+        '
+        Me.Timer7.Interval = 2500
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.ProgressBar1)
+        Me.Panel1.Controls.Add(Me.ProgLabel)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(190, 122)
+        Me.Panel1.TabIndex = 10
+        Me.Panel1.Visible = False
+        '
+        'ProgLabel
+        '
+        Me.ProgLabel.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.ProgLabel.AutoSize = True
+        Me.ProgLabel.Location = New System.Drawing.Point(21, 11)
+        Me.ProgLabel.Name = "ProgLabel"
+        Me.ProgLabel.Size = New System.Drawing.Size(150, 13)
+        Me.ProgLabel.TabIndex = 11
+        Me.ProgLabel.Text = "Connecting to Wunderground "
+        Me.ProgLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 91)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 1
+        Me.ProgressBar1.Maximum = 7
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(166, 23)
+        Me.ProgressBar1.TabIndex = 11
+        '
+        'Timer8
+        '
+        Me.Timer8.Interval = 120000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(190, 122)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WebControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Enter your info"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Private WithEvents WebControl1 As Awesomium.Windows.Forms.WebControl
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents Timer4 As System.Windows.Forms.Timer
+    Friend WithEvents Timer5 As System.Windows.Forms.Timer
+    Friend WithEvents Timer6 As System.Windows.Forms.Timer
+    Friend WithEvents Timer7 As System.Windows.Forms.Timer
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgLabel As System.Windows.Forms.Label
+    Friend WithEvents Timer8 As System.Windows.Forms.Timer
+
+End Class
---
+++ code/wunderground-api/Form1.vb
@@ -0,0 +1,155 @@
+﻿Public Class Form1
+
+    Public derp As String = ""
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer8.Start()
+        If TextBox1.Text = "" Then
+            MessageBox.Show("Please enter your first name!")
+            Exit Sub
+        End If
+        If TextBox2.Text = "" Then
+            MessageBox.Show("Please enter your last name!")
+            Exit Sub
+        End If
+        If ComboBox1.SelectedItem = Nothing Then
+            MessageBox.Show("Please select a country!")
+            Exit Sub
+        End If
+        Me.Hide()
+        For Each cont As Control In Me.Controls
+            If cont.Name.Contains("Form") = True Then Continue For
+            cont.Hide()
+        Next
+        Me.Text = "Generating key"
+        Dim rand As New Random()
+        derp = ""
+        For length As Integer = 0 To 5
+            derp = derp + Hex(Math.Round(rand.NextDouble * 255).ToString).ToString.ToLower
+        Next
+        WebControl1.Show()
+        WebControl1.Source = New Uri("http://www.wunderground.com/weather/api/d/login.html")
+        Panel1.Show()
+        Me.Show()
+    End Sub
+
+    Private Sub Awesomium_Windows_Forms_WebControl_DocumentReady(sender As Object, e As Awesomium.Core.UrlEventArgs) Handles WebControl1.DocumentReady
+        If e.Url.ToString.StartsWith("http://www.wunderground.com/weather/api/d/login.html?validation=1&email=") = True Then
+            ProgLabel.Text = "Validating account"
+            ProgressBar1.Value = 2
+            WebControl1.Source = New Uri("http://mailinator.com/inbox.jsp?to=" + derp)
+        ElseIf e.Url = New Uri("http://www.wunderground.com/weather/api/d/login.html") Then
+            Timer1.Start()
+        ElseIf e.Url.ToString.StartsWith("http://mailinator.com/inbox.jsp?to=") = True Then
+            Timer2.Start()
+        ElseIf e.Url.ToString.StartsWith("http://mailinator.com/rendermail.jsp") Then
+            WebControl1.ExecuteJavascript("window.location = document.getElementsByTagName('a')[0].href")
+        ElseIf e.Url.ToString.StartsWith("http://www.wunderground.com/weather/api/d/login.html?validation=") Then
+            Timer4.Start()
+        ElseIf e.Url = New Uri("http://www.wunderground.com/weather/api/") Then
+            WebControl1.Source = New Uri("http://www.wunderground.com/weather/api/d/pricing.html")
+        ElseIf e.Url = New Uri("http://www.wunderground.com/weather/api/d/pricing.html") Then
+            Timer5.Start()
+        ElseIf e.Url = New Uri("http://www.wunderground.com/weather/api/d/questionnaire.html") Then
+            Timer7.Start()
+        ElseIf e.Url.ToString.Contains("wunderground.recurly.com/subscribe//") Then
+            ProgLabel.Text = "Parsing key"
+            ProgressBar1.Value = 6
+            Dim key As String = e.Url.ToString
+            key = key.Substring(key.IndexOf("subscribe//") + 11)
+            key = key.Remove(key.IndexOf("/?email="))
+            ProgLabel.Text = "Done"
+            ProgressBar1.Value = 7
+            My.Computer.Clipboard.SetText(key)
+            MessageBox.Show("Your API key is " + Chr(34) + key + Chr(34) + ". It has been copied to your clipboard.")
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If WebControl1.IsLoading = True Then Exit Sub
+        ProgLabel.Text = "Creating account"
+        ProgressBar1.Value = 1
+        FillForm()
+        Timer1.Stop()
+    End Sub
+
+    Sub FillForm()
+        WebControl1.ExecuteJavascript("document.getElementById('create-email').value = '" + derp + "@mailinator.com'")
+        WebControl1.ExecuteJavascript("document.getElementById('create-pass').value = 'wunderground-bot'")
+        WebControl1.ExecuteJavascript("document.getElementById('create-pass2').value = 'wunderground-bot'")
+        WebControl1.ExecuteJavascript("document.getElementsByClassName('create')[3].value = '" + derp + "'")
+        WebControl1.ExecuteJavascript("document.getElementById('agree-to-terms').checked = true")
+        WebControl1.ExecuteJavascript("document.getElementById('enroll').submit();")
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If WebControl1.IsLoading = True Then Exit Sub
+        WebControl1.ExecuteJavascript("document.getElementsByTagName('a')[6].onclick();")
+        Timer2.Stop()
+        Timer3.Start()
+        'Dim emailcode As String =
+        '.Substring(WebControl1.HTML.IndexOf("showmail('") + 10)
+        'emailcode = emailcode.Remove(emailcode.IndexOf("')"))
+        'MessageBox.Show(emailcode)
+        'WebControl1.ExecuteJavascript("showmail('" + emailcode + "')")
+        'Timer2.Stop()
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        If WebControl1.IsLoading = True Then Exit Sub
+        WebControl1.ExecuteJavascript("window.location = document.getElementsByTagName('iframe')[0].src")
+        Timer3.Stop()
+        Timer2.Stop()
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        If WebControl1.IsLoading = True Then Exit Sub
+        Timer4.Stop()
+        ProgLabel.Text = "Signing into account"
+        ProgressBar1.Value = 3
+        WebControl1.ExecuteJavascript("document.getElementById('login-email').value = '" + derp + "@mailinator.com'")
+        WebControl1.ExecuteJavascript("document.getElementById('login-pass').value = 'wunderground-bot'")
+        WebControl1.ExecuteJavascript("document.getElementById('login').submit();")
+    End Sub
+
+    Private Sub Timer5_Tick(sender As Object, e As EventArgs) Handles Timer5.Tick
+        If WebControl1.IsLoading = True Then Exit Sub
+        Timer6.Start()
+        Timer5.Stop()
+    End Sub
+
+    Private Sub Timer6_Tick(sender As Object, e As EventArgs) Handles Timer6.Tick
+        ProgLabel.Text = "Completing account"
+        ProgressBar1.Value = 4
+        WebControl1.ExecuteJavascript("document.getElementsByName('api_package')[5].checked = true")
+        WebControl1.ExecuteJavascript("document.getElementsByName('api_history')[2].checked = true")
+        WebControl1.ExecuteJavascript("document.getElementsByName('api_usage')[0].checked = true")
+        WebControl1.ExecuteJavascript("document.getElementById('planConfig_r').submit();")
+        Timer6.Stop()
+    End Sub
+
+    Private Sub Timer7_Tick(sender As Object, e As EventArgs) Handles Timer7.Tick
+        If WebControl1.IsLoading = True Then Exit Sub
+        WebControl1.ExecuteJavascript("document.getElementById('contact_name').value = '" + TextBox1.Text + " " + TextBox2.Text + "'")
+        WebControl1.ExecuteJavascript("document.getElementsByName('contact_email')[0].value = '" + derp + "@mailinator.com'")
+        WebControl1.ExecuteJavascript("document.getElementsByName('project_name')[0].value = 'Watson'")
+        WebControl1.ExecuteJavascript("document.getElementsByName('website')[0].value = 'http://hackaday.io/project/2142'")
+        WebControl1.ExecuteJavascript("document.getElementsByName('where')[3].checked = true")
+        WebControl1.ExecuteJavascript("document.getElementsByName('commercial')[1].checked = true")
+        WebControl1.ExecuteJavascript("document.getElementsByName('chip')[1].checked = true")
+        WebControl1.ExecuteJavascript("document.getElementsByName('selectedCountry')[0].value = '" + ComboBox1.SelectedItem + "'")
+        WebControl1.ExecuteJavascript("document.getElementById('field').value = 'This account was automatically registered for a new user of the Watson home automation suite.'")
+        WebControl1.ExecuteJavascript("document.getElementsByName('branding')[0].checked = true")
+        WebControl1.ExecuteJavascript("document.getElementsByName('agree')[0].checked = true")
+        WebControl1.ExecuteJavascript("document.getElementsByTagName('form')[1].submit();")
+        ProgLabel.Text = "Obtaining key"
+        ProgressBar1.Value = 5
+        Timer7.Stop()
+    End Sub
+
+    Private Sub Timer8_Tick(sender As Object, e As EventArgs) Handles Timer8.Tick
+        MessageBox.Show("The app has timed out. Please relaunch this app to continue.")
+        Me.Close()
+    End Sub
+End Class
